432f37c84c38507e8b5d4e5498dc79b8c86cf6ae
Purushottam K.C <Kseapuru20@gmail.com>
2020-09-13 14:19:14 +0545

    template driven form practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularForm';
+  @ViewChild('f') formElement : NgForm
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male','female'];
+  submitted = false;
+  userInfo = {
+    username: '',
+    email: '',
+    gender: '',
+    question: '',
+    answer: ''
+  }
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.formElement.setValue(
+    //   {
+    //     UserAndEmail:{
+    //       username : suggestedName,
+    //       email: ''
+    //     },
+    //     answer: 'dummy',
+    //     secret: 'pet',
+    //     gender: 'female'
+    //   }
+    // )
+    this.formElement.form.patchValue({
+        UserAndEmail:{
+          username: suggestedName,
+          email: 'kseapuru@20.com'
+        }
+    })
+  }
+
+  // OnSubmit(formElement: NgForm) {
+  //   console.log(formElement);
+    
+  // }
+
+  OnSubmit(){
+    console.log(this.formElement);
+    this.submitted = true;
+    this.userInfo.username = this.formElement.value.UserAndEmail.username;
+    this.userInfo.email = this.formElement.value.UserAndEmail.email;
+    this.userInfo.answer = this.formElement.value.answer;
+    this.userInfo.question = this.formElement.value.secret;
+    this.userInfo.gender = this.formElement.value.gender;
+    this.formElement.reset();
+  }
 }
