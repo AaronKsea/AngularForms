25d7ebb16fba27474d0b87d91687065ee8aea863
Purushottam K.C <Kseapuru20@gmail.com>
2020-09-27 20:46:07 +0545

    reactive form implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,99 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('f') formElement : NgForm
-  defaultQuestion = 'teacher';
-  answer = '';
-  genders = ['male','female'];
-  submitted = false;
-  userInfo = {
-    username: '',
-    email: '',
-    gender: '',
-    question: '',
-    answer: ''
-  }
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  restrictedUsernames = ['Anju','Ranju'];
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.formElement.setValue(
-    //   {
-    //     UserAndEmail:{
-    //       username : suggestedName,
-    //       email: ''
-    //     },
-    //     answer: 'dummy',
-    //     secret: 'pet',
-    //     gender: 'female'
-    //   }
-    // )
-    this.formElement.form.patchValue({
-        UserAndEmail:{
-          username: suggestedName,
-          email: 'kseapuru@20.com'
+  signupForm: FormGroup;
+
+  ngOnInit(){
+    this.signupForm = new FormGroup(
+      {
+        'formUserData' : new FormGroup({
+          'username': new FormControl(null, [Validators.required, this.UserNameRestriction.bind(this)]) ,
+          'email': new FormControl(null, [Validators.required, Validators.email],this.EmailRestriction.bind(this))
+        }),
+        'gender': new FormControl('female'),
+        'hobbies': new FormArray([])
+      })
+      // this.signupForm.valueChanges.subscribe(
+
+      //   (value) => {
+      //     console.log(value);
+      //   }
+      // )
+
+      this.signupForm.statusChanges.subscribe(
+        (status) => {
+          console.log(status);
         }
-    })
-  }
+      )
 
-  // OnSubmit(formElement: NgForm) {
-  //   console.log(formElement);
-    
-  // }
+    // this.signupForm.setValue(
+    //   {
+    //     'formUserData': {
+    //       'username': 'Purur',
+    //       'email': 'kscae20@gmail.com'
+    //     },
+    //     'gender': 'male',
+    //     'hobbies': []
+    //   }
+    // );
+
+    this.signupForm.patchValue(
+      {
+        'formUserData': {
+          'username': 'Purur'
+        }
+      }
+    );
+  }
 
   OnSubmit(){
-    console.log(this.formElement);
-    this.submitted = true;
-    this.userInfo.username = this.formElement.value.UserAndEmail.username;
-    this.userInfo.email = this.formElement.value.UserAndEmail.email;
-    this.userInfo.answer = this.formElement.value.answer;
-    this.userInfo.question = this.formElement.value.secret;
-    this.userInfo.gender = this.formElement.value.gender;
-    this.formElement.reset();
+    console.log(this.signupForm);
+    this.signupForm.reset(
+      {'gender':'male'}
+    );
   }
+
+  OnAddHobby(){
+    const hobbyControl = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(hobbyControl);
+  }
+
+
+  UserNameRestriction(controls: FormControl): { [s: string]: boolean } {
+    if(this.restrictedUsernames.indexOf(controls.value) !== -1){
+      return {'name_restricted': true} //if return object then treated as error,error will be logged in form control
+    }
+    return null;//null is treated as valid entry
+  }
+
+  EmailRestriction(control: FormControl): Promise<any>| Observable<any>{
+    const promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(
+          () => {
+            if(control.value === 'test@gmail.com'){
+              resolve({'email_restricted': true})
+            }
+            else{
+              resolve(null);
+            }
+          },1500
+        )
+
+      }
+    )
+    return promise;
+
+  }
+
 }
